acf27b0ddd11db224a7efe9b2db124513d625d61
shubham <shubham007vaishala@gmail.com>
2026-03-07 16:26:17 +0530

    database connection setup

--- server.js
+++ server.js
@@ -1,34 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+// const Mobile = require("./src/models/productModel"); // your schema
+const productRout = require("./src/routes/productRoutes");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const products = [
-  {
-    id: 1,
-    name: "iPhone 15",
-    price: 80000,
-    category: "Mobile"
-  },
-  {
-    id: 2,
-    name: "Samsung S24",
-    price: 70000,
-    category: "Mobile"
-  },
-  {
-    id: 3,
-    name: "MacBook Air",
-    price: 120000,
-    category: "Laptop"
-  }
-];
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/quickDeal")
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ DB connection error:", err.message));
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+// Correct route path
+// app.get("/api/mobiles", async (req, res) => {
+//   try {
+//     const products = await Mobile.find();
+//     res.set("Cache-Control", "no-store");
+//     res.json(products);
+//   } catch (err) {
+//     console.error("Error fetching products:", err);
+//     res.status(500).json({ error: "Server error", details: err.message });
+//   }
+// });
+app.get("/api/mobiles", productRout);
 
 app.listen(5000, () => {
-  console.log("Server running on port 5000");
+  console.log("🚀 Server running on port 5000");
 });
